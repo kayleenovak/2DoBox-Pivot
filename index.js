68a540c2da2e733301bf01374ef438e6e80afb24
Kristen Hallstrom <klhalls89@gmail.com>
2018-09-08 14:47:55 -0600

    first pass at js

--- index.js
+++ index.js
@@ -1,53 +1,45 @@
-var title = $('#title-input').val();
-var body = $('#body-input').val();
-var numCards = 0;
+// var body = $('.body-input').val();
 var qualityVariable = "swill";
 
-var newCard = function(id , title , body , quality) {
-    return '<div id="' + id + '"class="card-container"><h2 class="title-of-card">'  
-            + title +  '</h2>'
-            + '<button class="delete-button"></button>'
-            +'<p class="body-of-card">'
-            + body + '</p>'
-            + '<button class="upvote"></button>' 
-            + '<button class="downvote"></button>' 
-            + '<p class="quality">' + 'quality:' + '<span class="qualityVariable">' + quality + '</span>' + '</p>'
-            + '<hr>' 
-            + '</div>';
+$('.save-btn').on('click', createIdea);
+
+function newCard(id, title, body, quality){
+ return `<article class="card-container" id='${id}'>
+            <h2 class="title-of-card">${title}</h2>
+            <button class="delete-button"></button>
+            <pclass="body-of-card">${body}</p>
+            <button class="upvote"></button>
+            <button class="downvote"></button>
+            <p class="quality">quality: ${quality}</p>
+          </article>`
+}
+
+function NewIdea(title, body) {
+    this.title = title;
+    this.body = body;
+    this.quality = 'Swill';
+    this.id = $.now();
 };
 
-function cardObject() {
-    return {
-        title: $('#title-input').val(),
-        body: $('#body-input').val(),
-        quality: qualityVariable
-    };
+function createIdea(event) {
+    event.preventDefault()
+    var newIdea = new NewIdea($('.title-input').val(), $('.body-input').val());
+    localStoreCard(newIdea);
+    $('.card-prepend').prepend(newCard(newIdea.id, newIdea.title, newIdea.body, newIdea.quality));
+}
+
+function localStoreCard(newIdea) {
+    var id = newIdea.id ;
+    localStorage.setItem(id, JSON.stringify(newIdea));
 }
 
 $.each(localStorage, function(key) {
-    var cardData = JSON.parse(this);
-    numCards++;
-    $( ".card-prepend" ).prepend(newCard(key, cardData.title, cardData.body, cardData.quality));
+    var retrieveIdea = localStorage.getItem(key);
+    var cardData = JSON.parse(retrieveIdea);
+   $( ".card-prepend" ).prepend(newCard(cardData.id, cardData.title, cardData.body, cardData.quality));
 });
 
-var localStoreCard = function() {
-    var cardString = JSON.stringify(cardObject());
-    localStorage.setItem('card' + numCards  , cardString);
-}
-
-$('.save-btn').on('click', function(event) {
-    event.preventDefault();
-    if ($('#title-input').val() === "" || $('#body-input').val() === "") {
-       return false;
-    };  
-
-    numCards++;
-    $( ".bottom-box" ).prepend(newCard('card' + numCards, $('#title-input').val(), $('#body-input').val(), qualityVariable)); 
-    localStoreCard();
-    $('form')[0].reset();
-});
-
-$(".bottom-box").on('click', function(event){
+$(".card-prepend").on('click', function(event){
     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
     var qualityVariable;
 
