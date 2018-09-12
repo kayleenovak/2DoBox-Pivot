f6e8757ea8a4bdeb523cae24e9c337c601455189
Kaylee Bunce <kaybunce@hotmail.com>
2018-09-12 17:01:56 -0600

    Switch idea classes to todo classes

--- index.js
+++ index.js
@@ -20,42 +20,42 @@ function createToDo(event) {
 }
 
 function delegateClick() {
-    if (event.target.classList.contains('upvote')) {
-        upvote(event);
-    } else if (event.target.classList.contains('downvote')) {
-        downvote(event);
-    } else if (event.target.classList.contains('delete-button')) {
-        deleteToDo(event);
-    } else if (event.target.classList.contains('complete-btn')) {
-        completeToDo(event);
-    }
+  if (event.target.classList.contains('upvote')) {
+    upvote(event);
+  } else if (event.target.classList.contains('downvote')) {
+    downvote(event);
+  } else if (event.target.classList.contains('delete-button')) {
+    deleteToDo(event);
+  } else if (event.target.classList.contains('complete-btn')) {
+    completeToDo(event);
+  }
 }
 
 function deleteToDo(event) {
-        var cardHTML = $(event.target).closest('.card-container').remove();
-        var cardHTMLId = cardHTML[0].id;
-        localStorage.removeItem(cardHTMLId);
+  var cardHTML = $(event.target).closest('.card-container').remove();
+  var cardHTMLId = cardHTML[0].id;
+  localStorage.removeItem(cardHTMLId);
 }
 
 function downvote(event) {
-    var arrayImportance = ['None', 'Low', 'Normal', 'High', 'Critical'];
-    var parsedIdea = JSON.parse(localStorage.getItem($(event.target).parent().parent().attr('id')));
-    if (parsedIdea.index >= 1) {
-        parsedIdea.index--
-        parsedIdea.importance = arrayImportance[parsedIdea.index];
-        $(event.target).siblings('.importance').text(`importance: ${parsedIdea.importance}`);
-    localStoreCard(parsedIdea);
-    }
+  var arrayImportance = ['None', 'Low', 'Normal', 'High', 'Critical'];
+  var parsedToDo = JSON.parse(localStorage.getItem($(event.target).parent().parent().attr('id')));
+  if (parsedToDo.index >= 1) {
+    parsedToDo.index--
+    parsedToDo.importance = arrayImportance[parsedToDo.index];
+    $(event.target).siblings('.importance').text(`importance: ${parsedToDo.importance}`);
+  localStoreCard(parsedToDo);
+  }
 }
 
 function editBody(event) {
   event.preventDefault();
   if ($(event.target).hasClass('body-of-card')) {
-  var id = $(event.target).parent().attr('id');
-  var parsedIdea = JSON.parse(localStorage.getItem(id));
-  var changeBody = event.target.innerText;
-  parsedIdea.body = changeBody;
-  localStoreCard(parsedIdea);
+    var id = $(event.target).parent().attr('id');
+    var parsedToDo = JSON.parse(localStorage.getItem(id));
+    var changeBody = event.target.innerText;
+    parsedToDo.body = changeBody;
+    localStoreCard(parsedToDo);
   } 
 }
 
@@ -70,11 +70,11 @@ function editBodyEnter (event) {
 function editIdea(event) {
   event.preventDefault();
   if ($(event.target).hasClass('title-of-card')) {
-  var id = $(event.target).parent().parent().attr('id');
-  var parsedIdea = JSON.parse(localStorage.getItem(id));
-  var changeTitle = event.target.innerText;
-  parsedIdea.title = changeTitle;
-  localStoreCard(parsedIdea);
+    var id = $(event.target).parent().parent().attr('id');
+    var parsedToDo = JSON.parse(localStorage.getItem(id));
+    var changeTitle = event.target.innerText;
+    parsedToDo.title = changeTitle;
+    localStoreCard(parsedToDo);
   }
 }
 
@@ -87,41 +87,41 @@ function editIdeaEnter (event) {
 }
 
 function localStoreCard(newToDo) {
-    var id = newToDo.id ;
-    localStorage.setItem(id, JSON.stringify(newToDo));
+  var id = newToDo.id ;
+  localStorage.setItem(id, JSON.stringify(newToDo));
 }
 
 function newCard(id, title, body, importance){
  return `<article class="card-container" id='${id}'>
             <div class="card-heading">
-                <h2 class="title-of-card" contenteditable>${title}</h2>
-                <button class="delete-button"></button>
+              <h2 class="title-of-card" contenteditable>${title}</h2>
+              <button class="delete-button"></button>
             </div>
-                <p class="body-of-card" contenteditable>${body}</p>
+            <p class="body-of-card" contenteditable>${body}</p>
             <div class="idea-body">
-                <button class="upvote"></button>
-                <button class="downvote"></button>
-                <p class="importance">importance: ${importance}</p>
-                <button class="complete-btn">Completed</button> 
+              <button class="upvote"></button>
+              <button class="downvote"></button>
+              <p class="importance">importance: ${importance}</p>
+              <button class="complete-btn">Completed</button> 
             </div>
           </article>`
 }
 
 function NewToDo(title, body) {
-    this.title = title;
-    this.body = body;
-    this.importance = 'Normal';
-    this.id = Date.now();
-    this.index = 2;
-    this.complete = false;
+  this.title = title;
+  this.body = body;
+  this.importance = 'Normal';
+  this.id = Date.now();
+  this.index = 2;
+  this.complete = false;
 };
 
 function onLoad() {
   for (var i = 0; i < localStorage.length; i++) {
-    var parsedIdea = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    var html = newCard(parsedIdea.id, parsedIdea.title, parsedIdea.body, parsedIdea.importance);
+    var parsedToDo = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    var html = newCard(parsedToDo.id, parsedToDo.title, parsedToDo.body, parsedToDo.importance);
     $('.card-prepend').prepend(html);
-    if (parsedIdea.complete === true) {
+    if (parsedToDo.complete === true) {
         $('.card-container').addClass('card-container-complete');
     }
   }
@@ -137,23 +137,23 @@ function search() {
 
 function toggleComplete(event) {
     var id = $(event.target).parent().parent().attr('id');
-    var parsedIdea = JSON.parse(localStorage.getItem(id));
+    var parsedToDo = JSON.parse(localStorage.getItem(id));
     if (event.target.parentNode.parentNode.classList.contains('card-container-complete')) {
-        parsedIdea.complete = true;
+        parsedToDo.complete = true;
     } else  {
-        parsedIdea.complete = false;
+        parsedToDo.complete = false;
     }
-    localStoreCard(parsedIdea);
+    localStoreCard(parsedToDo);
 }
 
 function upvote(event) {
     var arrayImportance = ['None', 'Low', 'Normal', 'High', 'Critical'];
-    var parsedIdea = JSON.parse(localStorage.getItem($(event.target).parent().parent().attr('id')));
-    if (parsedIdea.index <= 3) {
-        parsedIdea.index++
-        parsedIdea.importance = arrayImportance[parsedIdea.index];
-        $(event.target).siblings('.importance').text(`importance: ${parsedIdea.importance}`);
-    localStoreCard(parsedIdea);
+    var parsedToDo = JSON.parse(localStorage.getItem($(event.target).parent().parent().attr('id')));
+    if (parsedToDo.index <= 3) {
+        parsedToDo.index++
+        parsedToDo.importance = arrayImportance[parsedToDo.index];
+        $(event.target).siblings('.importance').text(`importance: ${parsedToDo.importance}`);
+    localStoreCard(parsedToDo);
     }
 }
 
